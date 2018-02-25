b9c09f456bfcdb628cc3ad2e03153c2ff85b9aa0
Henry Crofts <hwcrofts@gmail.com>
2018-02-25 13:11:08 +0000

    Working on Prototype build
    Henry

---
+++ Game/RhythmGame/Assets/Scripts/Activator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+
+    public KeyCode key;
+    public bool active = false;
+    GameObject node;
+
+    public Node nodee;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(key) && active)
+            nodee.Dir = 2;
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        active = true;
+        if (collision.gameObject.tag == "Node")
+        {
+            node = collision.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        active = false;
+    }
+}
---
+++ Game/RhythmGame/Assets/Scripts/Node.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed;
+    public int Dir = 1;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Start () {
+    rb.velocity = new Vector2(0, -speed);
+	}
+	
+	void Update () {
+        SetDirection();
+	}
+
+    void SetDirection()
+    {
+        if (Dir == 1)
+        {
+            rb.velocity = new Vector2(0, -speed);
+        }
+        else if (Dir == 2)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+    }
+}
